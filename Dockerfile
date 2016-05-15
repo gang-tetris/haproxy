52ff24f097093547d3d2b63ef3ce7b069d49af05
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-15 23:45:18 +0300

    Added ability to setup port to bind

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM haproxy:1.6
 COPY haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
 ENV BACKEND="backend"
 ENV PORT="8080"
+ENV EXPOSE_PORT="80"
 ENV SERVERS=""
 COPY add_servers.sh /add_servers.sh
 
--- add_servers.sh
+++ add_servers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed "s/BACKEND/${BACKEND}/g" -i /usr/local/etc/haproxy/haproxy.cfg
+sed "s/EXPOSE_PORT/${EXPOSE_PORT}/g" -i /usr/local/etc/haproxy/haproxy.cfg
 for name in $SERVERS
 do
     echo "    server ${name} ${name}:${PORT} maxconn 32 check" \
